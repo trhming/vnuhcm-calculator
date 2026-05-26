020f2f882aaac1c4dbf50ff7af7ee7ca09b57dc7
Minh Tran <101495295+trhming@users.noreply.github.com>
2026-05-26 17:15:56 +0700

    Update App.jsx to fix speed insights

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { Analytics } from '@vercel/analytics/react';
-import { SpeedInsights } from "@vercel/speed-insights/next"
+import { SpeedInsights } from "@vercel/speed-insights/react"
 import { Layout } from './components/layout/Layout';
 import { Home } from './pages/Home';
 import { HcmusCalculator } from './pages/HcmusCalculator';
@@ -32,7 +32,7 @@ function App() {
         </Route>
       </Routes>
       <Analytics />
-      <SpeedInsights/>
+      <SpeedInsights />
     </BrowserRouter>
   );
 }

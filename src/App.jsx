dcb546f6c3521480319c01c38ef7a80cab82142d
Minh Tran <thminh.1804@gmail.com>
2026-05-27 12:30:33 +0700

    change to reduce edge requests

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,5 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { Analytics } from '@vercel/analytics/react';
-import { SpeedInsights } from "@vercel/speed-insights/react"
 import { Layout } from './components/layout/Layout';
 import { Home } from './pages/Home';
 import { HcmusCalculator } from './pages/HcmusCalculator';
@@ -32,7 +31,6 @@ function App() {
         </Route>
       </Routes>
       <Analytics />
-      <SpeedInsights />
     </BrowserRouter>
   );
 }
